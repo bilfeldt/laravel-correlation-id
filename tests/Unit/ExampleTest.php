c1c4b3eaba5cd8ea0a099389a1c389f1276e2200
Anders Bilfeldt <abilfeldt@gmail.com>
2023-09-27 15:56:43 +0200

    Add example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bilfeldt\CorrelationId\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
