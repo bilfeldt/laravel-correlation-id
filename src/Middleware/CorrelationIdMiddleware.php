5be255bff405d28bd18f5a63aded1c14b4db7b14
Anders Bilfeldt <abilfeldt@gmail.com>
2023-10-12 09:44:31 +0200

    StyleCI

--- src/Middleware/CorrelationIdMiddleware.php
+++ src/Middleware/CorrelationIdMiddleware.php
@@ -14,7 +14,7 @@ class CorrelationIdMiddleware
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
